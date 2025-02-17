819b73578ae5a5a93a5fa8640bf59d81a6a81384
andrii <a.iakumiv@gmail.com>
2025-02-17 12:53:37 +0200

    Add function filterArrray for homework task-3

--- js/task-02.js
+++ js/task-02.js
@@ -1,7 +1,6 @@
 function makeArray(firstArray, secondArray, maxLength) {
   const newArray = firstArray.concat(secondArray);
-  const newArrayLength = newArray.length;
-  if (newArrayLength > maxLength) {
+  if (newArray.length > maxLength) {
     return newArray.slice(0, maxLength);
   } else {
     return newArray;
